cd8f358983bbfbdc6d2c513ba1a5523422422adf
RhizoNymph <quantnymph@gmail.com>
2026-03-22 01:29:54 -0700

    fix: add missing healthchecks, fix monitoring URLs, fix MinIO console port and env vars

--- src/compose/service_templates/storage.rs
+++ src/compose/service_templates/storage.rs
@@ -15,9 +15,7 @@ pub fn minio(config: &ServiceConfig) -> Result<(dct::Service, IndexMap<String, S
 
     let svc = dct::Service {
         image: Some(format!("minio/minio:{version}")),
-        command: Some(dct::Command::Simple(format!(
-            "server /data --console-address :{console_port}"
-        ))),
+        command: Some(dct::Command::Simple("server /data --console-address :9001".to_string())),
         ports: dct::Ports::Short(vec![
             format!("{host_port}:9000"),
             format!("{console_port}:9001"),
@@ -42,8 +40,8 @@ pub fn minio(config: &ServiceConfig) -> Result<(dct::Service, IndexMap<String, S
 
     let agent_env = IndexMap::from([
         ("S3_ENDPOINT".to_string(), format!("http://minio:9000")),
-        ("AWS_ACCESS_KEY_ID".to_string(), "${MINIO_ACCESS_KEY:-minioadmin}".to_string()),
-        ("AWS_SECRET_ACCESS_KEY".to_string(), "${MINIO_SECRET_KEY:-minioadmin}".to_string()),
+        ("S3_ACCESS_KEY_ID".to_string(), "${MINIO_ACCESS_KEY:-minioadmin}".to_string()),
+        ("S3_SECRET_ACCESS_KEY".to_string(), "${MINIO_SECRET_KEY:-minioadmin}".to_string()),
     ]);
 
     Ok((svc, agent_env))
